3e79de61248b3a9f25767b8c429f0cc8f110ab74
MalloZup <dmaiocchi@suse.com>
2018-05-24 20:15:05 +0200

    fixup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,6 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "backins_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
