09fd9401a65a5cacd4f36f1112cd436c7d4701b0
MalloZup <dmaiocchi@suse.com>
2018-05-27 20:55:24 +0200

    use join data exp

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,9 +2,9 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
     @jobs_uniq_name = Job.select(:name).distinct
-    @ordered_tests_result = Hash.new
+    @ordered_tests_result_by_jobname = Hash.new
     @jobs_uniq_name.each do |job|
-      @ordered_tests_result[job.to_s] = Job.where("name = ?", job['name']).order(:build_timestamp)
+      @ordered_tests_result_by_jobname[job.to_s] = Job.where("name = ?", job['name']).order(:build_timestamp)
     end 
   end
 end
