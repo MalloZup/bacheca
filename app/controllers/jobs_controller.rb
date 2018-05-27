03a91ddbd6bf45786cc91abc93fe7f0e7e6ec32a
MalloZup <dmaiocchi@suse.com>
2018-05-27 16:51:05 +0200

    add filtered results

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,5 +2,9 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
     @jobs_uniq_name = Job.select(:name).distinct
+    @ordered_tests_result = Hash.new
+    @jobs_uniq_name.each do |job|
+      @ordered_tests_result[job.to_s] = Job.where("name = ?", job['name']).order(:build_timestamp)
+    end 
   end
 end
