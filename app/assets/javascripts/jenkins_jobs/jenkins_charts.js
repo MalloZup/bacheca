f466770218069d2bd4091710a74dc581744169a9
MalloZup <dmaiocchi@suse.com>
2018-05-28 19:07:19 +0200

    move charts to library

---
+++ app/assets/javascripts/jenkins_jobs/jenkins_charts.js
@@ -0,0 +1,35 @@
+// options for line_chart 
+var linechart_options =  {
+    interpolateNulls: true,
+    hAxis: {
+      title: 'Timestamp',
+      textStyle: {
+        color: '#01579b',
+        fontSize: 24,
+        fontName: 'Arial',
+        bold: true,
+        italic: true
+      },
+      titleTextStyle: {
+        color: '#01579b',
+        fontSize: 24,
+        fontName: 'Arial',
+        bold: true,
+        italic: true
+      }
+    },
+    vAxis: {
+      title: 'tests failures',
+      textStyle: {
+        color: '#1a237e',
+        fontSize: 24,
+        bold: true
+      },
+      titleTextStyle: {
+        color: '#1a237e',
+        fontSize: 24,
+        bold: true
+      }
+    },
+    colors: ['#a52714', '#097138']
+};
