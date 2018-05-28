20b23e1e7394c29bf88d347cde23a60e1b81bdd8
MalloZup <dmaiocchi@suse.com>
2018-05-28 12:54:25 +0200

    improve js code

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,7 +4,8 @@ class JobsController < ApplicationController
     @jobs_uniq_name = Job.select(:name).distinct
     @ordered_tests_result_by_jobname = Hash.new
     @jobs_uniq_name.each do |job|
-      @ordered_tests_result_by_jobname[job.to_s] = Job.where("name = ?", job['name']).order(:build_timestamp)
-    end 
+      job_name = job['name']
+      @ordered_tests_result_by_jobname[job_name] = Job.where("name = ?", job['name']).order(:build_timestamp).pluck(:build_timestamp, :tests_failed, :build_date)
+    end
   end
 end
