2200681e1c49c09fbc4266c342342002d5216287
MalloZup <dmaiocchi@suse.com>
2018-05-31 16:10:14 +0200

    update whenever file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,4 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
 every 30.minute do
    rake "db:migrate"
    rake "db:seed"
-   command '/usr/bin/echo updated_db_!!'
 end
