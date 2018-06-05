5160d25ea7eb34cb89b1ff40958148bcebd95d52
MalloZup <dmaiocchi@suse.com>
2018-06-05 17:57:51 +0200

    fix whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,4 @@
+ENV.each { |k, v| env(k, v) }
 every 15.minute do
   command '/bacheca/bin/rails db:migrate &> /tmp/bacheca_migrate_logs'
   command '/bacheca/bin/rails db:seed &> /tmp/bacheca_seed_log '
