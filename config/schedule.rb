79a07283be6085d1728d083916297757830baa95
MalloZup <dmaiocchi@suse.com>
2018-06-05 11:49:55 +0200

    add logs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 every 15.minute do
-  command 'cd /bacheca && rake db:migrate'
-  command 'cd /bacheca && rake db:seed'
+  command 'cd /bacheca && rake db:migrate &> /tmp/bacheca_migrate_logs'
+  command 'cd /bacheca && rake db:seed &> /tmp/bacheca_seed_log '
   command "date > /tmp/current_date_bacheca"
 end
