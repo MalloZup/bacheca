f2951438021c469bc337921505a5b5dba41149d6
MalloZup <dmaiocchi@suse.com>
2018-06-05 18:07:07 +0200

    fix rake

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 ENV.each { |k, v| env(k, v) }
 every 15.minute do
-  command '/bacheca/bin/rails db:migrate &> /tmp/bacheca_migrate_logs'
-  command '/bacheca/bin/rails db:seed &> /tmp/bacheca_seed_log '
+  command 'cd /bacheca && rake db:migrate &> /tmp/bacheca_migrate_logs'
+  command 'cd /bacheca && rake db:seed &> /tmp/bacheca_seed_log '
   command "date > /tmp/current_date_bacheca"
 end
