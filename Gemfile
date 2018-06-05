df2c2309bf809436df80c47cb8a7baf4807790dc
MalloZup <dmaiocchi@suse.com>
2018-06-05 17:11:23 +0200

    add rails cmd for docker

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ## Min version of ruby required.
 # ruby ">= 2.3.1"
-gem 'rake', '~>  12.3.1'
+gem 'rake'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 # Use sqlite3 as the database for Active Record
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 every 15.minute do
-  command 'cd /bacheca && rake db:migrate &> /tmp/bacheca_migrate_logs'
-  command 'cd /bacheca && rake db:seed &> /tmp/bacheca_seed_log '
+  command '/bacheca/bin/rails db:migrate &> /tmp/bacheca_migrate_logs'
+  command '/bacheca/bin/rails db:seed &> /tmp/bacheca_seed_log '
   command "date > /tmp/current_date_bacheca"
 end
